dd045181ebe9fa5b70ff777291a7328dd263bd5d
aish.dande9 <aish.dande@gmail.com>
2024-09-02 13:43:56 -0500

    graded assignment-6

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,26 @@
+import './styling.css';
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
-     </tr>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
+       </tr>
      </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     <tbody> 
+      {sat.map(
+        (data,id) => (<tr key={id}>
+          <th><td>{data.name}</td></th>
+          <th><td>{data.type}</td></th>
+          <th><td>{data.launchDate}</td></th>
+          <th><td>{data.operational ? 'Active' : 'Inactive'}</td></th>
+        
+        </tr>)
+      )}
+     
      </tbody>
    </table>
   );
