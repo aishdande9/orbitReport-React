51139b5805624e31e69d83f3033952c16f4b2546
aish.dande9 <aish.dande@gmail.com>
2024-09-04 13:02:24 -0500

    changes made!

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,19 +1,15 @@
-
 import satData from "./satData";
-import './styling.css';
+import "./styling.css";
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  return (
+    <div className="flex-container">
+      {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      ))}
 
-
- return (
- <div className = "flex-container">
-  
-    {displaySats.map(
-          (sat,id)=><button onClick={()=>filterByType(sat)} key={id}>{sat} Orbit</button>
-  )}
-      
-     
-      <button onClick={()=>setSat(satData)} >All Orbits</button>
-    
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,28 +1,26 @@
-import './styling.css';
+import "./styling.css";
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Name</th>
-       <th>Type of Satellite</th>
-       <th>Launch Date</th>
-       <th>Status</th>
-       </tr>
-     </thead>
-     <tbody> 
-      {sat.map(
-        (data,id) => (<tr key={id}>
-          <th><td>{data.name}</td></th>
-          <th><td>{data.type}</td></th>
-          <th><td>{data.launchDate}</td></th>
-          <th><td>{data.operational ? 'Active' : 'Inactive'}</td></th>
-        
-        </tr>)
-      )}
-     
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sat.map((data, id) => (
+          <tr key={id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational ? "Active" : "Inactive"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
